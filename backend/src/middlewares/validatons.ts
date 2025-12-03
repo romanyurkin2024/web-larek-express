2b9f4c57e45621ef72024e40410e05612adb01e9
romanyurkin2024 <tsoyroman2024@yandex.kz>
2025-12-03 09:36:05 +0500

    Добавил enum для HttpStatus

--- backend/src/middlewares/validatons.ts
+++ backend/src/middlewares/validatons.ts
@@ -5,6 +5,18 @@ enum PaymentMethod {
   Card = 'card',
   Online = 'online'
 }
+export enum HttpStatus {
+  Ok = 200,
+  Created = 201,
+  NoContent = 204,
+  MultipleChoices = 300,
+  BadRequest = 400,
+  Unauthorized = 401,
+  Forbidden = 403,
+  NotFound = 404,
+  Conflict = 409,
+  InternalServerError = 500,
+}
 
 export const validateOrderBody = celebrate({
   [Segments.BODY]: Joi.object().keys({
